c9b6b95add7eb4d69edabcc224e88c5d3e00c717
amkumar072 <amkumar072@gmail.com>
2022-10-26 21:04:46 +0100

    updated script file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Create Keyspace query
+CREATE KEYSPACE bookings WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;
+
+// Create table query
 CREATE TABLE bookings.bookings (
   bookingref text PRIMARY KEY,
   container_size int,
